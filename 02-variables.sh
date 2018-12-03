8ef7c0a7a8e3ca83a56e026c5b4873e1d0d587b5
Nicolas <Nicolas.lb@outlook.com>
2018-12-03 10:02:49 -0500

    Modif du 02

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+#pas d'espace sinon sa marche pas
+prenom=Nicolas
+echo $prenom
+nomDeFamille="L a b i n e"
+echo $nomDeFamille
+
+#Forcer une declaration d'une variable "int"
+declare -i age=33
+
+#Mettre une variable READ only
+declare -r age2=33
+
+#Les variables sytèmes
+#Nom de l'executable
+echo $0
+#argument 1
+echo $1
+#argument 2 ( on peut continuer jusqu'a 9 )
+echo $2
+#Nombre d'argument passer
+echo $#
