ebf239198802210cee02d891a496700e07621290
Nicolas <Nicolas.lb@outlook.com>
2018-12-03 09:49:41 -0500

    Modification du 01

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# Ceci est un commentaire
+echo "Hello world"
+
+#-n empeche les retour de ligne
+echo -n "Vive "
+echo "Linux"
+
+#-e pour d'utiliser les caractere speciau comme le \n
+echo -e  "123\n123\n123"
+
+#crée une variable et fait son echo ( -p permet de mettre un message)
+#met dekoi dans une variable pas de signe de $ mais quand on veut la valeur faut mettre le signe de $
+read -p "Vous avez quel âge?" age
+echo $age "ans! Vous paraissez plus jeune"
+
