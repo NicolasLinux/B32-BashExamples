59dafbb414b85b27927fd1be06abb517f460d7e1
Nicolas <Nicolas.lb@outlook.com>
2018-12-03 10:52:02 -0500

    Modif du 05

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+#met le resultat d'une commande dans une variable
+liste=`ls`
+
+#for each
+for fic in $liste
+do
+	echo $fic
+done
+
+resultat=o
+
+#-o = or, -a = and
+while test $resultat = o -o $resultat = O
+do
+	read -p "Entrer o pour répéter lla boucle: " resultat
+done
