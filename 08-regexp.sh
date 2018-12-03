e210f70306a469b6dec205cd68416d95a35abab0
Nicolas <Nicolas.lb@outlook.com>
2018-12-03 11:10:11 -0500

    Modif du 08

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo -n "Teste : "
+read texte
+
+#^ = commence et $ pour fini donc seulement des chiffre dans l'exemple
+# ~ sert a dire qu'il va y avoir une expression reg
+if [[ $texte =~ ^[0-9]+$ ]];then
+	echo "Numerique"
+fi
