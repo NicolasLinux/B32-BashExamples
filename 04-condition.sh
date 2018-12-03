f6405ec6376ceae1a8242a352807275dc988dea2
Nicolas <Nicolas.lb@outlook.com>
2018-12-03 10:39:50 -0500

    Modif du 04

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
+read -p "Entrez votre note: " note
 
+if test $note -lt 60
+then
+	echo "Désoler, vouz devez reprendre le cours..."
+elif [[ $note -eq 60 ]]
+then
+	echo "Vous etes a la limte !!"
+else
+	echo "Bravo!"
+fi
 
+read -p "Entrez une lettre : " lettre
 
+case $lettre in
+#savoir si c'est une minuscule
+	[[:lower:]])
+		echo "minuscule"
+		;;
+#cas defaut
+	*)
+		echo "autre..."
+		;;
+#ferme le case
+esac
+
+#pas egal : != et egla : =
+if test $lettre != "a"
+then
+	echo "La lettre n'est pas a"
+fi
 
 
 
