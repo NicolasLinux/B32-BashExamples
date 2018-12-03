094012e1d6d98a6c4d63e20317269e7b38e2d857
Nicolas <Nicolas.lb@outlook.com>
2018-12-03 11:17:20 -0500

    Modif du 09

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#met un chemin de fichier en argument et avec cela on test voir s'il existe ou pas
+# ex : ./09-fichier.sh /bin/bash
+if test -e $1
+then
+	echo "existe"
+#-d regarde si c'est un répertoire
+	if test -d $1
+	then
+		echo "Il est un répertoire"
+	fi
+else
+	echo "existe pas"
+fi
+
