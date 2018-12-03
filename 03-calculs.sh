5c8748e8325018bda58d0096f9ac48b0d266178a
Nicolas <Nicolas.lb@outlook.com>
2018-12-03 10:09:40 -0500

    Modif de 03

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+#affiche 10+19
+echo 10+19
+#affiche le resultat du calcul
+echo $((10+19))
+
+#Prend les valeur comme un cin pour faire un calcul
+read -p "Nombre 1:" premier
+read -p "Nombre 2:" second
+let resultat=$premier*$second
+echo $resultat
+
+
